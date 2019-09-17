3e1ab3466dcac2558e767eaa1928108358de6806
Patrick Lafrance <patrick.lafrance@gmail.com>
2019-09-17 11:59:06 -0400

    Création de Grille avec toString

---
+++ src/quebec/crosemont/g04/Grille.java
@@ -0,0 +1,41 @@
+package quebec.crosemont.g04;
+
+import java.util.Random;
+
+class Grille{
+    
+    public String toString(){
+	String res="   ";
+	for(int i=0;i<largeur;i++){
+	    res+=i+(i<10?"  ":" ");
+	}
+	res+="\n  ╔";
+	for(int i=0;i<largeur-1;i++){
+	    res+="══╦";
+	}
+	res+="══╗\n 0";
+	for(int j=0;j<hauteur;j++){
+	    for(int i=0;i<largeur;i++){
+		res+="║" + getFaceCase(i,j);
+	    }
+	    res+="║";
+	    if(j==hauteur-1) break;
+
+	    res+="\n  ╠";
+
+	    for(int i=0;i<largeur-1;i++){
+		res+="══╬";
+	    }
+	    res+="══╣\n" +(j<9?" "+(j+1):(j+1));
+
+	}
+	res+="\n  ╚";
+	for(int i=0;i<largeur-1;i++){
+	    res+="══╩";
+	}
+	res+="══╝";
+
+	return res;
+    }
+}
+
