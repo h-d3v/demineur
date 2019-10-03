53c33704f30bc0b3a2cd1e81b49812ba11cec5e5
Julien Jacquard <jacquardjul@gmail.com>
2019-10-03 08:34:30 -0400

    Modification de la classe Grill Modif de la methode faceCase qui retourne desormais les cases decouvertes
    Ajout d'asserts sur la methode compterVoisins(), initialiser()
    
    la methode initialiser() appelle desormais la methode decouvrir lorsqu'elle est appellee
    
    La methode decouvrir() decouvre la case x, y entree en parametre et decouvre aussi les cases voisines si elle n'ont pas de bombes
    
    La methode toutReveler decouvre toutes les cases sauf les bombes
    
    La methode getFaceCase() reourne la representation en String d'une case selon le modele : si la case est decouverte et n'est pas une bombe, elle affiche le nombre de bombes voisines sinon elle montre sa marque ou son type selon l'avancement du jeu
    
    Quelques tests d'affichage sur le programme main Test.java
    Gestion des exceptions a faire, test junit a faire, modifier les classes et les methodes selon les resultats
    
    IMPORTANT modifier les try catch (juste utilises de maniere temporaire)dans les methodes par des boucles et des condtions

--- src/quebec/crosemont/g04/demineur/Test.java
+++ src/quebec/crosemont/g04/demineur/Test.java
@@ -8,12 +8,12 @@ public class Test {
         uneGrille.initialiser(-1,-1,20);
         System.out.println(uneGrille);
 
-        System.out.println("Test unitaire de la fonction compterVoisins() pour les cases8-7 9-9 0-0 \n");
+        System.out.println("Test d'affichage de la fonction compterVoisins() pour les cases8-7 9-9 0-0 \n");
 
         System.out.println(uneGrille.compterVoisins(8,7));
         System.out.println(uneGrille.compterVoisins(9,9));
         System.out.println(uneGrille.compterVoisins(0,0));
-        System.out.println("Test unitaire de la fonction marquer() de la classe Case");
+        System.out.println("Test d'affichage de la fonction marquer() de la classe Case");
         uneGrille.cases[1][1].marquer();
         System.out.println(uneGrille);
         uneGrille.cases[1][1].marquer();
@@ -31,7 +31,7 @@ public class Test {
         uneGrille.decouvrir(2,2);
         System.out.println(uneGrille);
 
-        System.out.println("Test unitaire de la fonction toutReveler()");
+        System.out.println("Test affichage de la fonction toutReveler()");
         uneGrille.toutReveler();
         System.out.println(uneGrille);
 
