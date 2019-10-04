fabe3f074049c52011e9b472251d122e188fed0c
Julien Jacquard <jacquardjul@gmail.com>
2019-10-04 11:03:32 -0400

    modif du test GrilleTest

--- test/quebec/crosemont/g04/demineur/GrilleTest.java
+++ test/quebec/crosemont/g04/demineur/GrilleTest.java
@@ -8,16 +8,18 @@ import static org.junit.Assert.*;
 
 public class GrilleTest {
 
-    Grille TestConstruteurSansAttribut= new Grille();
+    Grille testConstruteurSansAttribut= new Grille();
 
     @Test
     public void getLargeurConstruteurSansAttribut() {
-        assertEquals(10, 10);
+        assertEquals(10, testConstruteurSansAttribut.getLargeur());
     }
 
     @Test
     public void getHauteurConstruteurSansAttribut() {
+        assertEquals(10, testConstruteurSansAttribut.getHauteur());
     }
+
 
     @Test
     public void getCase() {
