72bce85f394ca49b6063ece557fb57883d9911ba
Julien Jacquard <jacquardjul@gmail.com>
2019-10-04 11:00:52 -0400

    Ajout des fichiers tests a completer

---
+++ test/quebec/crosemont/g04/demineur/GrilleTest.java
@@ -0,0 +1,49 @@
+package quebec.crosemont.g04.demineur;
+
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrilleTest {
+
+    Grille TestConstruteurSansAttribut= new Grille();
+
+    @Test
+    public void getLargeurConstruteurSansAttribut() {
+        assertEquals(10, 10);
+    }
+
+    @Test
+    public void getHauteurConstruteurSansAttribut() {
+    }
+
+    @Test
+    public void getCase() {
+    }
+
+    @Test
+    public void getFaceCase() {
+    }
+
+    @Test
+    public void compterVoisins() {
+    }
+
+    @Test
+    public void initialiser() {
+    }
+
+    @Test
+    public void decouvrir() {
+    }
+
+    @Test
+    public void toutReveler() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
