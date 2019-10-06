9b55069f665c12c80ca7a04d1be95c00d50815f2
Hedi O <1756859@crosemont.qc.ca>
2019-10-06 11:59:02 -0700

    Ajout du traitement d'erreur au main

--- src/quebec/crosemont/g04/demineur/main.java
+++ src/quebec/crosemont/g04/demineur/main.java
@@ -4,7 +4,7 @@
  * 
  * 
  */
-
+ 
 import java.util.Random;
 import java.util.*;
 import java.io.*;
@@ -12,17 +12,42 @@ import java.io.*;
 public class main{
 	
 	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in); 
 		int largeur=0,hauteur=0;
-		boolean etatGrille=false;
-		largeur=ES.lireEntier("Veuillez entrez la largeur  de grille choisie: ");
-		hauteur=ES.lireEntier("Veuillez entrez la hauteur de grille choisie: ");
+		boolean erreurEntree=true;
+		//tant qu'un numero valide n'est pas entre, le programme n'avance pas
+		do{
+			try{
+				System.out.println("Veuillez entrez la largeur  de grille choisie: ");
+				largeur = Integer.parseInt(in.next());
+				System.out.println("Veuillez entrez la hauteur de grille choisie: ");
+				hauteur = Integer.parseInt(in.next());
+				erreurEntree=false;
+			}catch(NumberFormatException e){
+				System.out.println("\n Veuillez entree un numero entier positif svp \n");
+				in.reset();
+				}  
+			} while(erreurEntree);
+		
 		int nbCases=largeur*hauteur;
 		
-		//tant que la grille ne respecte pas les bonnesDimensions(entre 5 et 2 147 483 647),le jeu ne commence pas ;
-		while(nbCases<5 || nbCases>2147483647){
-			System.out.println("Veuillez faire une grille qui aura entre 5 et 2 147 483 647 cases");
-			largeur=ES.lireEntier("Veuillez entrez la largeur  de grille choisie: ");
-			hauteur=ES.lireEntier("Veuillez entrez la hauteur de grille choisie: ");
+		//tant que la grille ne respecte pas les bonnesDimensions(entre 25 et 2 147 483 647),le jeu ne commence pas ;
+		while(hauteur<5 || largeur<5 || nbCases<25 || nbCases>2147483647){
+			System.out.println("Veuillez faire une grille qui aura entre 25 et 2 147 483 647 cases \n avec une largeur et hauteur minimale de 5 cases");
+			
+			erreurEntree=true;
+			do{
+				try{
+					System.out.println("Veuillez entrez la largeur  de grille choisie: ");
+					largeur = Integer.parseInt(in.next());
+					System.out.println("Veuillez entrez la hauteur de grille choisie: ");
+					hauteur = Integer.parseInt(in.next());
+					erreurEntree=false;
+				}catch(NumberFormatException e){
+					System.out.println("\n Veuillez entree un numero entier positif svp \n");
+					in.reset();
+					}  
+				} while(erreurEntree);
 			nbCases=largeur*hauteur;
 			
 	}
@@ -30,9 +55,9 @@ public class main{
 		Case[][] casesJeu=GrilleJeu.cases;
 		int nbBombe=(largeur*hauteur)/10;
 		
-		
-		
-		
 	}
 }
+	
+		
+
 
