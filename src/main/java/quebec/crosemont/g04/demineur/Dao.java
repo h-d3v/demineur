f4b788fa7ae9f1dd4e2d8999ed64d45871378455
Hedi <1756859@crosemont.qc.ca>
2019-11-04 17:48:30 -0500

    Modification DAO.java

--- src/main/java/quebec/crosemont/g04/demineur/Dao.java
+++ src/main/java/quebec/crosemont/g04/demineur/Dao.java
@@ -2,11 +2,12 @@ package quebec.crosemont.g04.demineur;
 
 public interface Dao<T> {
 
+
     T lire(final Object id) throws DAOException;
 
-    T ajouter(final T entite) throws DAOException;
+    public T ajouter(final T entite) throws DAOException;
 
-    void supprimer(final T entite) throws DAOException;
+    public void supprimer(final T entite) throws DAOException;
 
-    T modifier(final T entite) throws DAOException;
+    public T modifier(final T entite) throws DAOException;
 }
